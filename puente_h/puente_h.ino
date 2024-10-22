07e50b52847a072037b80bc453b8af45cfb4f653
Toni <100935955+tonialcaraz@users.noreply.github.com>
2024-10-22 14:33:13 +0200

    Create puente_h.ino

---
+++ puente_h/puente_h.ino
@@ -0,0 +1,43 @@
+/*
+ *  Taloselectronics
+ *  soporte@taloselectronics.com 
+ *  Rafael Lozano Rolón
+*/
+
+#define input_a 2
+#define pwm 3
+#define input_b 4
+void setup()
+{
+    pinMode(input_a, OUTPUT);
+    pinMode(input_b, OUTPUT);
+    
+}
+void loop()
+{
+    //Giro a la derecha
+    digitalWrite(input_a, HIGH);
+    digitalWrite(input_b, LOW);
+
+    //aumenta la velocidad hasta maximo 255
+    for (int i = 0; i < 255; i++)
+    {
+        analogWrite(pwm, i);
+        delay(20);
+    }
+
+    //Giro a la derecha
+    digitalWrite(input_a, LOW);
+    digitalWrite(input_b, HIGH);
+
+    //Disminuye velocidad
+    for (int i = 255; i > 0; i--)
+    {
+        analogWrite(pwm, i);
+        delay(20);
+    }
+
+    //Esperar 1 segundo antes de volver a acelerar
+    delay(1000);
+    
+}
